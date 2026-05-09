7e0fa6b9fa8e96bcb5707eed7ea92cd5e568e70e
Gyurkovics Feri <gyurkovicsferi@gmail.com>
2026-05-09 09:18:38 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/midi-editor/' : '/',
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
